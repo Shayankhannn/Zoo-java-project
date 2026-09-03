3c9c802915b26aae472937f5d576c47922a84bd3
shayankhannn <shayannkhannn007@gmail.com>
2026-09-03 09:14:40 -0700

    Project - zoo using java

---
+++ src/Dolphin.java
@@ -0,0 +1,41 @@
+import javax.tools.DiagnosticCollector;
+
+public class Dolphin extends Animal implements Swim{
+    private String color;
+    private int swimmingSpeed;
+
+    public Dolphin() {
+        super("Dolphin");
+    }
+
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getSwimmingSpeed() {
+        return swimmingSpeed;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setSwimmingSpeed(int swimmingSpeed) {
+        this.swimmingSpeed = swimmingSpeed;
+    }
+
+    @Override
+    public void eatingFood() {
+        System.out.println("Dolphin: I am eating delicious fish");
+    }
+
+    @Override
+    public void eatingCompleted() {
+        System.out.println("I have eaten fish");
+    }
+    @Override
+    public void swimming() {
+        System.out.println("Dolphin: I am swimming at the speed of " + swimmingSpeed + " nautical miles per hour");
+    }
+}
